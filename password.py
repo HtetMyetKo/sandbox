2ecf56166373c35df55312a6cd86735da83ca17c
HtetMyetKo <htetmyetko@my.jcu.edu.au>
2021-11-28 22:25:39 +0630

    Add Password checking function

---
+++ password.py
@@ -0,0 +1,15 @@
+"""
+Check login credentials
+
+Password checker. Created by Cyrus. 25-11-2021
+"""
+
+def main():
+    """Start program"""
+    password = "12345"
+    if password == "12345":
+        print("Logged in")
+    else:
+        print("Illegal access")
+
+main()
